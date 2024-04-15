68bdf78dad9d23b9d7a798094b94210cb811773b
lilianmarey <mareylilian@gmail.com>
2024-04-15 16:11:14 +0000

    created predict script

--- run.py
+++ run.py
@@ -1,17 +1,18 @@
 from recbole.quick_start import run_recbole
 
 if __name__ == "__main__":
-    for platform in ["LFM", "DEEZER"]:
-        for country in ["GLOBAL", "FR", "DE", "BR"]:
-            for model in ["ItemKNN", "NeuMF"]:
-                run_recbole(
-                    model=model,
-                    dataset=platform + "_" + country,
-                    config_dict={
-                        "field_separator": ",",
-                        "platform": platform,
-                        "country": country,
-                        "topk": [10, 100],
-                        "valid_metric": "MRR@10",
-                    },
-                )
+    for _ in range(100):
+        for platform in ["LFM", "DEEZER"]:
+            for country in ["FR", "DE", "BR", "GLOBAL"]:
+                for model in ["ItemKNN", "NeuMF"]:
+                    run_recbole(
+                        model=model,
+                        dataset=platform + "_" + country,
+                        config_dict={
+                            "field_separator": ",",
+                            "platform": platform,
+                            "country": country,
+                            "topk": [10, 100],
+                            "valid_metric": "MRR@10",
+                        },
+                    )
